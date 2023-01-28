73fb3d9efe170da3f9b717eebc14923fd43257a2
misterq <illsi@naver.com>
2023-01-29 00:08:52 +0900

    게임 패드 버튼 설정

--- Source/Tenetris/PlayerController/TenetrisPlayerController.cpp
+++ Source/Tenetris/PlayerController/TenetrisPlayerController.cpp
@@ -20,4 +20,17 @@ void ATenetrisPlayerController::SetupInputComponent()
 	UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("RotateCounterClockWise", EKeys::LeftControl));
 	UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("HardDrop", EKeys::SpaceBar));
 	UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("Hold", EKeys::LeftShift));
+
+	UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("StartMoveLeft", EKeys::Gamepad_DPad_Left));
+	UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("StopMoveLeft", EKeys::Gamepad_DPad_Left));
+	UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("StartMoveRight", EKeys::Gamepad_DPad_Right));
+	UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("StopMoveRight", EKeys::Gamepad_DPad_Right));
+
+	UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("StartSoftDrop", EKeys::Gamepad_DPad_Down));
+	UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("StopSoftDrop", EKeys::Gamepad_DPad_Down));
+
+	UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("RotateClockWise", EKeys::Gamepad_FaceButton_Right));
+	UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("RotateCounterClockWise", EKeys::Gamepad_FaceButton_Bottom));
+	UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("HardDrop", EKeys::Gamepad_DPad_Up));
+	UPlayerInput::AddEngineDefinedActionMapping(FInputActionKeyMapping("Hold", EKeys::Gamepad_LeftShoulder));
 }
