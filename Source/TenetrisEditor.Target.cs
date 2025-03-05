af5067aa220218130b6accc957c0d1bf10ac99f6
규 서문 <illsi@naver.com>
2025-03-05 15:30:55 +0900

    엔진 버전 업그레이드 5.1 -> 5.5

--- Source/TenetrisEditor.Target.cs
+++ Source/TenetrisEditor.Target.cs
@@ -8,7 +8,7 @@ public class TenetrisEditorTarget : TargetRules
 	public TenetrisEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		ExtraModuleNames.AddRange( new string[] { "Tenetris" } );
 	}
 }
